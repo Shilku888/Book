62e67d3902d58909c05d9e9433d29357f48e2974
Shilku888 <shilpikumari088@gmail.com>
2021-08-16 00:08:59 +0530

    initial cpmmit

---
+++ augapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from augapp.models import Books,Author
+
+# Register your models here.
+
+admin.site.register(Books)
+admin.site.register(Author)
---
+++ augapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AugappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'augapp'
